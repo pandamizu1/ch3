163faec96309bf9074868a29b013db040f04dc7b
pandamizu1 <panda.shiro077@gmail.com>
2025-05-21 11:56:19 +0700

    Fix dark mode implementation

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -16,10 +16,8 @@ const ThemeToggle = () => {
       {theme === 'dark' ? (
         <Sun className="w-5 h-5 text-yellow-500" />
       ) : (
-        <Moon className="w-5 h-5 text-gray-600" />
+        <Moon className="w-5 h-5 text-gray-600 dark:text-gray-400" />
       )}
     </motion.button>
   );
 };
-
-export default ThemeToggle;
